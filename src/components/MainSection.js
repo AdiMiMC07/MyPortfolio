0757bd7ae7e5d1913d2fb214a9a0128671178c9b
Aditya Mishra <adityavishu03@gmail.com>
2023-04-02 11:38:53 +0530

    Enhanced the front page

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import logo from '../Vector 1.svg'
 import '../styles/home.css'
+import satellite from '../images/satellite.png'
 const MainSection = () => {
     return (
         <>
@@ -8,13 +8,25 @@ const MainSection = () => {
                 <div className="container">
                     <div className="base">
                         <div className="intro-cont">
+                            <div className="position" style={{ backgroundColor: "rgba(0,0,0,0)" }}>
+                                <div className="viewbox">
+
+                                </div>
+                                <div className="viewbox-2">
+
+                                </div>
+                                <div className="gradient"></div>
+                            </div>
+                            <div className="satellite">
+                                <img src={satellite} className="satellite" alt="" />
+                            </div>
                             <p id="name">ADITYA MISHRA</p>
                             <p id="intro-para">
                                 Hi, I am Aditya Mishra,<br />
                                 Frontend Developer and a sophomore majoring in
                                 Mathematics and
                                 Computer Science</p>
-                            <img className="logo-img pballs" data-speed="0.71" src={logo} alt="" />
+                            {/* <img className="logo-img pballs" data-speed="0.71" src={logo} alt="" /> */}
                         </div>
                     </div>
                 </div>
